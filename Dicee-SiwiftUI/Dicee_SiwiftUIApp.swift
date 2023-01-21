0b1feeeeeb9551a2a7599a6110b2a9cec833421a
Zülfücan Karakuş <84273117+zulfucankarakus@users.noreply.github.com>
2023-01-21 23:08:02 +0300

    Initial Commit

---
+++ Dicee-SiwiftUI/Dicee_SiwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SiwiftUIApp.swift
+//  Dicee-SiwiftUI
+//
+//  Created by Zülfücan Karakuş on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SiwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
